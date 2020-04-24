7a900bb620fbfbe63e1a68f417908c02a92d1012
VanLien1503 <hoanglien89nd@gmail.com>
2020-04-24 13:50:25 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Dòng Code Đầu Tiên Tôi Viết Trên Java");
+    }
+}
